62946f6300ecb1e965b197bfe20fdbd073ef3210
Andrey Shaposhnikov <36455862+MagisterUnivers@users.noreply.github.com>
2023-03-16 10:46:26 +0200

    Delete src directory

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const totalCategory = document.querySelectorAll('.item');
-console.log('Number of categories: ' + totalCategory.length);
-
-[...totalCategory].forEach((item) => {
-	console.log(`Category:  ${item.querySelector('h2').textContent}`);
-	console.log(`Elements:  ${item.querySelector('ul').children.length}`);
-});
--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const dragMe = document.querySelector('#font-size-control');
-const camelCase = document.querySelector('#text');
-
-camelCase.style.fontSize = `56px`;
-
-dragMe.addEventListener('input', () => {
-	camelCase.style.fontSize = `${dragMe.value}px`;
-	console.log(dragMe.value);
-});
